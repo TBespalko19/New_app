42f94834e0cf096820f9ffebb8eddf23197926a7
TBespalko19 <tbespalko19@gmail.com>
2018-09-28 22:41:24 +0300

    Updated app4 and add app5

---
+++ My_App1/lesson_08/app5.rb
@@ -0,0 +1,39 @@
+(-2...2).each do |x|
+	puts x
+end
+
+puts "============"
+
+
+(0...5).each do |x|
+	puts x
+end
+
+puts "============"
+
+(10...11).each do |x|
+	puts x
+end
+
+("a".."z").each do |x|
+	puts x
+end
+
+puts "============"
+
+("aa".."bb").each do |x|
+	puts x
+end
+
+puts "============"
+
+("cde".."fff").each do |x|
+	puts x
+end
+
+puts "============"
+
+("10aa".."20bb").each do |x|
+
+end
+
