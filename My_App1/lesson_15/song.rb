e42495a0ff11262121eb4bcfd2b94df811a369b8
TBespalko19 <tbespalko19@gmail.com>
2018-10-03 15:49:18 +0300

    Add lesson 15 part 2

--- My_App1/lesson_15/song.rb
+++ My_App1/lesson_15/song.rb
@@ -1,5 +1,16 @@
-# class Artist
-# end
+class Artist
+	attr_accessor :name, :album
+
+	def initialize name
+		@name = name
+		@albums = []
+		@artist = []
+	end
+
+	def add_album album
+		@artist << album
+	end
+end
 
 class Album
 	attr_reader :name, :songs
@@ -24,14 +35,22 @@ class Song
 	end
 end
 
-album = Album.new 'Innuendo'
+artist = Artist.new 'Queen'
+
+album1 = Album.new 'Innuendo'
+album2 = Album.new 'Innuendo1'
 
 song1 = Song.new 'The show must go on', 6
 song2 = Song.new 'Ride the wild wind', 4
 song3 = Song.new 'Innuendo', 6
 
-album.add_song song1
-album.add_song song2
-album.add_song song3
+album1.add_song song1
+album1.add_song song2
+album1.add_song song3
 
-puts album.songs[1].name
+puts album1.songs[1].name
+
+artist.add_album album1
+artist.add_album album2
+
+puts artist.add_album album2.name
