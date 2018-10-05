583632d8a78464d4a501d97019ad8f9c6e5ab858
TBespalko19 <tbespalko19@gmail.com>
2018-10-05 18:30:33 +0300

    Add lesson 18 part 1

---
+++ My_App1/lesson_18/demo2.rb
@@ -0,0 +1,25 @@
+def is_password_weak password
+
+	input = File.open "password.txt", "r"
+
+	while (line = input.gets)
+		line.strip!
+
+		if password.include? line
+			# puts "Your password is weak"
+			# exit
+			return rtue
+		end
+		#	puts line
+	end
+	return false
+end
+
+print "Enter password: "
+my_password = gets.strip
+
+if is_password_weak my_password
+puts "Your password is weak"
+else
+puts "Your password is not weak"
+end
