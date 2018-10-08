e9d250ea19ab70121782d76c8b83a8d5b70c1c12
TBespalko19 <tbespalko19@gmail.com>
2018-10-08 16:44:55 +0300

    Add lesson 20

--- My_App1/lesson_19/app1.rb
+++ My_App1/lesson_19/app1.rb
@@ -1,7 +1,38 @@
 require 'sinatra'
 
 get '/' do
-	'Hi'
+	erb :index
+end
+
+get '/contacts' do
+	under_construction
+	# @title = 'Contacts'
+	# @message = 'Phone number: 111222'
+	
+	# erb :message
+end
+
+get '/fag' do
+	under_construction
+	# @title = 'FAQ'
+	# @message = 'Under construction'
+	
+	# erb :message
+end
+
+get '/something' do
+	under_construction
+	# @title = 'Something'
+	# @message = 'Bla-bla'
+	
+	# erb :message
+end
+
+def under_construction
+	@title = 'Under construction'
+	@message = 'This page is under construction'
+	
+	erb :message
 end
 
 post '/' do
@@ -10,7 +41,10 @@ post '/' do
 
 	if @login == 'admin' && @password == 'secret'
 		erb :welcome
+	elsif @login == 'admin' && @password == 'admin'
+		@message = 'Haha, nice try! Access denied!'
 	else
+		@message = 'Access denied!!!'
 		erb :index
 	end
 end
