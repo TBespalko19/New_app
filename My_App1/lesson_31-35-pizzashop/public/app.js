3ef6055184e458732d8ac5c71a6bd7f828c2b15b
TBespalko19 <tbespalko19@gmail.com>
2018-10-20 23:32:56 +0300

    Add lesson 33 part 2

--- My_App1/lesson_31-35-pizzashop/public/app.js
+++ My_App1/lesson_31-35-pizzashop/public/app.js
@@ -1,11 +1,20 @@
-	function something()
-	{
-		var x = window.localStorage.getItem('bbb') // x = hh['bb']
+function something()
+{
+	var x = window.localStorage.getItem('bbb') // x = hh['bb']
 
-		x = x * 1 + 1 // x = x + 1
+	x = x * 1 + 1 // x = x + 1
 
-		window.localStorage.setItem('bbb', x ) // hh['bbb'] = x
+	window.localStorage.setItem('bbb', x ) // hh['bbb'] = x
 
-		alert(x);
-	}
+	alert(x);
+}
 
+
+function add_to_cart(id)
+{
+	var key = 'product' + id
+	// alert('You added pizza with id: + id');
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
+}
