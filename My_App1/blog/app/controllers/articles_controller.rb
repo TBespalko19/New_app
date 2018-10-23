7109177648598ba4529175220edac112bb3a4a9d
TBespalko19 <tbespalko19@gmail.com>
2018-10-24 01:20:00 +0300

    Add lesson 39

--- My_App1/blog/app/controllers/articles_controller.rb
+++ My_App1/blog/app/controllers/articles_controller.rb
@@ -1,10 +1,45 @@
 class ArticlesController < ApplicationController
-# дoбавить action new
-def new
-end
 
-def create
-	render plain: params[:article].inspect
-end
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def new
+	end
+
+	def create
+		@article = Article.new(article_params)
+
+		if @article.save
+			redirect_to @article
+		else
+			render action: 'new'
+		end
+	#	render plain: params[:article].inspect
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render action: 'edit'
+		end
+	end
+
+	private
+
+	def article_params
+		params.require(:article).permit(:title, :text)
+	end
 
 end
